40e99c40d9cc7e53ccd187933975fc5f5a873840
Michelle Leong <leong.michellew@gmail.com>
2023-04-18 18:57:55 -0700

    starting adding stuff to dynamic page

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { type AnimeType, type DataType } from "../../types";
 
 const Details = () => {
-  const [ anime, setAnime ] = useState<AnimeType | null>(null);
+  const [anime, setAnime] = useState<AnimeType | null>(null);
   const router = useRouter();
   const { id } = router.query;
   useEffect((): void => {
     const getAnimeDetails = async (): Promise<void> => {
       try {
         const res = await fetch(
-          `https://api.jikan.moe/v4/anime/${id ? id.toString() : '1'}`
+          `https://api.jikan.moe/v4/anime/${id ? id.toString() : "1"}`
         );
         const data = (await res.json()) as DataType;
         setAnime(data.data);
@@ -23,14 +23,18 @@ const Details = () => {
     };
     void getAnimeDetails();
   }, [id]);
-  return(
-    <main className = 'h-screen w-screen flex flex-col justify-center items-center'>
-      <p className = 'text-black'>Anime: {id}</p>
-      {anime && 
-      <p className = 'text-black'>{anime.title}</p>
-      
-      }
+  return (
+    <main className="flex h-screen w-screen flex-col items-center justify-center">
+      <p className="text-black">Anime: {id}</p>
+      {anime && <p className="text-black">{anime.title}</p>}
+      {anime && <p>{anime.title_english}</p>}
     </main>
+    // score, ranking, popularity, members
+    //add to list
+    //season and year?
+    //genres
+    //synopsis
+    //image
   );
 };
 
